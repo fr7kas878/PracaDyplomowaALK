7eb5efd4a5f9532718351e7fe71caf0f99a7ec14
fr7kas878 <fr7kas@gmail.com>
2026-04-16 20:52:13 +0000

    podzielenie krokow testu na osobne metody, zeby mozna bylo uzywac powtarzajacych sie krokow

--- FakeStoreTESTS/Selenium_tests/happy_path/test_05_buyCouponPayment_happyPath.py
+++ FakeStoreTESTS/Selenium_tests/happy_path/test_05_buyCouponPayment_happyPath.py
@@ -12,8 +12,10 @@ class BuyingHP(BaseTest):
     def setUp(self):
         super().setUp()
 
-    # @unittest.skip("Temporary skipping")
-    def test_buy_coupon_payment(self):
+        self.BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+        self.DATA_PATH = os.path.join(self.BASE_DIR, 'data')
+
+    def add_products_to_cart(self):
         # 1. primary menu on main page - click Sklep
         self.driver.find_element(By.XPATH, '//*[@id="menu-item-198"]').click()
         # 2. choose a first category - windsurfing[1]
@@ -33,40 +35,43 @@ class BuyingHP(BaseTest):
             EC.element_to_be_clickable
             ((By.CSS_SELECTOR, '.added_to_cart.wc-forward')) ).click()
 
+    def apply_coupon(self):
         # 5. enter a coupon code and click a button "Zastosuj kupon"
         # 6. use coupons from csv file
-        file_path = os.path.join(os.path.dirname(__file__), '/FakeStoreTESTS/data/couponsTest.csv')
-
+        file_path = os.path.join(self.DATA_PATH, 'couponsTest.csv')
         # 7. take a valid code for this category
         with open(file_path, newline='') as csvfile:
-            reader = csv.reader(csvfile) # tu jest zwykly reader csv - ktory bierze liste a nie slownik
+            reader = csv.reader(csvfile)  # tu jest zwykly reader csv - ktory bierze liste a nie slownik
             rows = list(reader)
-            coupon_code =rows[7][0]
+            coupon_code = rows[7][0]
 
-        print (f' W tym tescie wybieramy kod dla kategorii windsurfing " {coupon_code} "- test pozytywny')
-        #8. wait to insert a code
+        print(f' W tym tescie wybieramy kod dla kategorii windsurfing " {coupon_code} "- test pozytywny')
+        # 8. wait to insert a code
         self.wait.until(
-            EC.visibility_of_element_located((By.NAME,'coupon_code'))
+            EC.visibility_of_element_located((By.NAME, 'coupon_code'))
         ).send_keys(coupon_code)
-        #9. click apply a coupon
+        # 9. click apply a coupon
         self.wait.until(
-            EC.element_to_be_clickable((By.NAME,'apply_coupon'))
+            EC.element_to_be_clickable((By.NAME, 'apply_coupon'))
         ).click()
-        #10 - check displayed message
+        # 10 - check displayed message
         message_element = self.wait.until(
-            EC.visibility_of_element_located((By.CLASS_NAME,'woocommerce-message' ))
+            EC.visibility_of_element_located((By.CLASS_NAME, 'woocommerce-message'))
         )
-        #11. Check an expected result:
+        # 11. Check an expected result:
         actual_msg = message_element.text
-
         self.assertIn('Kupon został pomyślnie użyty', actual_msg)
+
+    def go_to_payment(self):
         # 12. Click a button "przejdz do platnosci"
         self.wait.until(
-            EC.element_to_be_clickable((By.CLASS_NAME, 'checkout-button.button.alt.wc-forward'))).click()
+            EC.element_to_be_clickable((By.CSS_SELECTOR, '.checkout-button.button.alt.wc-forward'))).click()
         # 13. Check expected result - redirecting to subpage .../zamowienie/
         self.wait.until(EC.url_contains('zamowienie'))
         current_url = self.driver.current_url
         self.assertEqual(current_url, "https://fakestore.testelka.pl/zamowienie/")
+
+    def fill_user_data(self):
         # 14. Fulfill an order data
         # 14a. email - from email generator
         generated_email = UserData.DATA_EMAIL
@@ -89,12 +94,14 @@ class BuyingHP(BaseTest):
         # 14.g phone_number
         phone = UserData.DATA_PHONE
         self.driver.find_element(By.ID, 'billing_phone').send_keys(phone)
+
+    def make_payment(self):
         # 15 FAKE CARD PAYMENT DATA
 
-        file_path = '/FakeStoreTESTS/data/credit_cards.csv'
+        file_path = os.path.join(self.DATA_PATH, 'credit_cards.csv')
 
         with open(file_path, newline='') as csvfile:
-            reader = list(csv.DictReader(csvfile))  # other csv reader with list option for random
+            reader = list(csv.DictReader(csvfile))
             row = random.choice(reader)
 
             number = row["number"]
@@ -102,24 +109,26 @@ class BuyingHP(BaseTest):
             cvv = row["cvv"]
 
             print(f' W tym tescie wylosowano karte kredytowa o parametrach: {number}, {expiry_date}, {cvv}')
-            # 16.  iframe structure for private stripe like p-numberInput
-            self.wait.until(
-                EC.frame_to_be_available_and_switch_to_it((By.CSS_SELECTOR, "iframe[name^='__privateStripeFrame']")))
-            # 17. data insert
-            self.wait.until(EC.visibility_of_element_located((By.ID, "payment-numberInput"))).send_keys(number)
-            self.wait.until(EC.visibility_of_element_located((By.ID, "payment-expiryInput"))).send_keys(expiry_date)
-            self.wait.until(EC.visibility_of_element_located((By.ID, "payment-cvcInput"))).send_keys(cvv)
+        # 16.  iframe structure for private stripe like p-numberInput
+        self.wait.until(
+            EC.frame_to_be_available_and_switch_to_it((By.CSS_SELECTOR, "iframe[name^='__privateStripeFrame']")))
+        # 17. data insert
+        self.wait.until(EC.visibility_of_element_located((By.ID, "payment-numberInput"))).send_keys(number)
+        self.wait.until(EC.visibility_of_element_located((By.ID, "payment-expiryInput"))).send_keys(expiry_date)
+        self.wait.until(EC.visibility_of_element_located((By.ID, "payment-cvcInput"))).send_keys(cvv)
 
-            # 18. checkbox - I accept terms of...
-            # back from iframe Stripe !
-            self.driver.switch_to.default_content()
-            # click checkbox
-            checkbox = self.wait.until(
-                EC.element_to_be_clickable((By.ID, "terms")))
-            if not checkbox.is_selected():
-                checkbox.click()
-            # 19. click button
-            self.wait.until(EC.element_to_be_clickable((By.ID, 'place_order'))).click()
+        # 18. checkbox - I accept terms of...
+        # back from iframe Stripe !
+        self.driver.switch_to.default_content()
+        # click checkbox
+        checkbox = self.wait.until(
+            EC.element_to_be_clickable((By.ID, "terms")))
+        if not checkbox.is_selected():
+            checkbox.click()
+        # 19. click button
+        self.wait.until(EC.element_to_be_clickable((By.ID, 'place_order'))).click()
+
+    def check_order(self):
         # 20. check excepted result - order number
         order_number = ""
         self.wait.until(EC.url_contains(order_number))
@@ -127,4 +136,11 @@ class BuyingHP(BaseTest):
         self.assertIn(order_number, current_url)
         print(f' Numer zamowienia to : {order_number}')
 
-
+    # @unittest.skip("Temporary skipping")
+    def test_buy_coupon_payment(self):
+        self.add_products_to_cart()
+        self.apply_coupon()
+        self.go_to_payment()
+        self.fill_user_data()
+        self.make_payment()
+        self.check_order()
---
+++ FakeStoreTESTS/Selenium_tests/happy_path/test_15_zeroQtyUpdateCart.py
@@ -0,0 +1,52 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from FakeStoreTESTS.Selenium_tests.happy_path.base_test import BaseTest
+
+
+class ProductsInCart(BaseTest):
+
+    def setUp(self):
+        super().setUp()
+
+    # @unittest.skip("Temporary skipping")
+    def test_add_to_cart(self):
+        # 1. primary menu on main page - click Sklep
+        self.driver.find_element(By.XPATH, '//*[@id="menu-item-198"]').click()
+        # 2. choose a first category - windsurfing[1]
+        element = self.wait.until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="main"]/ul/li[1]//a')) )
+        element.click()
+        # 3. add to cart several products -->adding several products in a loop by product_id :
+        products = ["386", "393", "391", "4116", "389" ]
+        for product_id in products:
+            self.wait.until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, f'[data-product_id="{product_id}"]')) ).click()
+        # 4.click button "Zobacz koszyk" to go to cart
+        self.wait.until(
+            EC.element_to_be_clickable
+            ((By.CSS_SELECTOR, '.added_to_cart.wc-forward')) ).click()
+        # 5. check expected result -> summary ordered quantity of produts
+
+        quantities = self.driver.find_elements(By.CSS_SELECTOR, ".qty")
+        total_qty = sum(int(q.get_attribute("value")) for q in quantities)
+
+        assert total_qty == len(products)
+        quantities = self.driver.find_elements(By.CSS_SELECTOR, ".qty")
+        total_qty = sum(int(q.get_attribute("value")) for q in quantities)
+
+        assert total_qty == len(products)
+
+        #6. set quantity of product to 0
+        qty_input = self.wait.until(
+            EC.visibility_of_element_located((By.CSS_SELECTOR, "input.qty")))
+        qty_input.clear()
+        qty_input.send_keys("0")
+
+        # 7. Click button "Zaktualizuj koszyk"
+        self.wait.until( EC.element_to_be_clickable((By.NAME, "update_cart")) ).click()
+
+        # 8. Check expected result - product is not visible in a cart anymore
+        updated_qty = self.wait.until(
+            EC.visibility_of_element_located((By.CSS_SELECTOR, "input.qty")) ).get_attribute("value")
+
+        self.assertEqual(updated_qty, "0")
