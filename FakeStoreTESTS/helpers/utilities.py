f8a6f889771868e1960845845dba0a2856b6ea07
fr7kas878 <fr7kas@gmail.com>
2026-04-20 19:29:09 +0000

    dodanie uiHelpers do zamykania banneru

--- FakeStoreTESTS/helpers/utilities.py
+++ FakeStoreTESTS/helpers/utilities.py
@@ -1,4 +1,6 @@
 from faker import Faker
+from selenium.webdriver.common.by import By
+
 faker = Faker()
 
 def generate_email():
@@ -6,3 +8,15 @@ def generate_email():
 
 def generate_password():
     return faker.password()
+
+class UIHelpers:
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def hide_banner(self):
+        try:
+            banner = self.driver.find_element(By.CLASS_NAME, "demo_store")
+            self.driver.execute_script("arguments[0].style.display='none';", banner)
+        except:
+            pass
