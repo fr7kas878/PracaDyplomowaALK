b8edfdb464659a39990515d7e673bee9a1791ab2
fr7kas878 <fr7kas@gmail.com>
2026-06-12 19:39:33 +0000

    poprawa testu02

--- tests/Selenium_tests/happy_path/test_02_register_validation_email_psswd.py
+++ tests/Selenium_tests/happy_path/test_02_register_validation_email_psswd.py
@@ -1,91 +1,83 @@
-from selenium.webdriver.common.by import By
 from faker import Faker
 
-from data.userdata import UserData, DataToLogIn
-from selenium.webdriver.support import expected_conditions as EC
+from data.userdata import UserData
+from data.userdata import DataToLogIn
+
+from pages.home_page import HomePage
+from pages.my_account_page import MyAccountPage
+
 from tests.Selenium_tests.happy_path.base_test import BaseTest
 
 
 class RegisterValidateFields(BaseTest):
 
     def setUp(self):
+
         super().setUp()
+
         self.faker = Faker()
 
-    #@unittest.skip("Temporary skipping")
-    #case1 - browser  wrong email validation when password is correct
+        self.home_page = HomePage(
+            self.driver
+        )
+
+        self.my_account = MyAccountPage(
+            self.driver
+        )
+
+    # case1 - browser wrong email validation when password is correct
     def test_invalid_email_browser_validation(self):
-        driver = self.driver
 
         #1.page Moje konto (menu)
-        self.wait.until(
-            EC.element_to_be_clickable((By.XPATH, '//*[@id="menu-item-201"]'))
-        ).click()
+        self.home_page.open_my_account()
 
-        # 1a - banner fix again
-        try:
-            self.driver.execute_script(
-                "document.querySelector('.woocommerce-store-notice')?.remove();"
-            )
-        except:
-            pass
+        self.assertTrue(
+            self.home_page.is_my_account_opened()
+        )
 
         #2.email wrong
-        email_input = self.wait.until(
-            EC.visibility_of_element_located((By.ID, 'reg_email'))
-        )
-        email_input.send_keys(DataToLogIn.DATA2_WRONGEMAIL)
-
-        #3.when password is correct
-        self.wait.until(
-            EC.visibility_of_element_located((By.ID, 'reg_password'))
-        ).send_keys(UserData.DATA_PASSWORD)
-
-        # klick register button
-        button = self.wait.until(
-            EC.element_to_be_clickable((By.CLASS_NAME, "woocommerce-form-register__submit"))
-        )
-        button.click()
-
-        #check if browser validation message is appearing - this is a construction for browser validator in JavaScript,
-        #because we ask a browser tooltip directly, not a running selenium webdriver by searching elements
-        validation_message = driver.execute_script(
-            "return arguments[0].validationMessage;", email_input
+        self.my_account.fill_registration_form(
+            DataToLogIn.DATA_WRONGEMAIL,
+            UserData.DATA_PASSWORD
         )
 
-        #6.check if browser validation message is not empty
-        self.assertTrue(len(validation_message) > 0)
+        #3.register
+        self.my_account.click_register()
 
-    #case2 - browser validation - to short password
+        #4.browser validation
+        validation_message = (
+            self.my_account.get_email_validation_message()
+        )
+
+        #5.check if browser validation message is not empty
+        self.assertNotEqual(
+            validation_message,
+            ""
+        )
+
+    # case2 - browser validation - to short password
     def test_short_password_validation(self):
-        driver = self.driver
 
         #1.page Moje konto (menu)
-        self.wait.until(
-            EC.element_to_be_clickable((By.XPATH, '//*[@id="menu-item-201"]'))
-        ).click()
+        self.home_page.open_my_account()
 
-        #1a. try if banner appears after refreshing web
-        try:
-            self.driver.execute_script(
-                "document.querySelector('.woocommerce-store-notice')?.remove();"
-            )
-        except:
-            pass
+        self.assertTrue(
+            self.home_page.is_my_account_opened()
+        )
 
         #2.email
-        self.wait.until(
-            EC.visibility_of_element_located((By.ID, 'reg_email'))
-        ).send_keys(self.faker.email())
+        self.my_account.fill_registration_form(
+            self.faker.email(),
+            "123"
+        )
 
-        #3.password (too short)
-        self.wait.until(
-            EC.visibility_of_element_located((By.ID, 'reg_password'))  ).send_keys("123")
+        #3.only check the password strenght
+        password_strength = (
+            self.my_account.get_password_strength()
+        )
 
-        #only check the password strenght - without registration
-        password_strength = self.wait.until(
-            EC.visibility_of_element_located(
-                (By.CSS_SELECTOR, ".woocommerce-password-strength")  )  )
-
-        #check if text contains word slabe
-        self.assertIn("słabe", password_strength.text.lower())
+        #4.check if text contains word 'slabe'
+        self.assertIn(
+            "słabe",
+            password_strength.lower()
+        )
